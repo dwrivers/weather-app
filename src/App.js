ade20571e0ff36a5b9414a1142d728cac7fc85cd
danielrivers97 <rivers@calpoly.edu>
2019-07-09 10:53:52 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+Import React from 'react';
+  class App extends React.Component {
+   render() {
+    return (
+     <div>
+      //add components here
+     </div>
+    ) 
+  }
+}
+export default App;
